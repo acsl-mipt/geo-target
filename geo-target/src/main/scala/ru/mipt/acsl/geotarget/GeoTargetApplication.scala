5d23a83216db5214484a8f103d8b253934b460cf
Artem Shein <metadeus@aisys.ru>
2015-12-11 01:37:10 +0300

    Add ScalaFX, port main App class to ScalaFX

--- geo-target/src/main/scala/ru/mipt/acsl/geotarget/GeoTargetApplication.scala
+++ geo-target/src/main/scala/ru/mipt/acsl/geotarget/GeoTargetApplication.scala
@@ -2,23 +2,36 @@ package ru.mipt.acsl.geotarget
 
 import java.util.Locale
 
-import javafx.application.Application
-import javafx.stage.Stage
 import c10n.C10N
 import c10n.annotations.DefaultC10NAnnotations
-import com.typesafe.scalalogging.LazyLogging
+import com.typesafe.scalalogging.{StrictLogging, LazyLogging}
+import gov.nasa.worldwind.layers.Layer
 import org.apache.commons.lang3.exception.ExceptionUtils
-import ru.cpb9.geotarget.ui.GeoTargetModel
-import ru.cpb9.geotarget.ui.controls.WorldWindNode
+import ru.cpb9.geotarget.ui.controls.parameters.flightdevice.ArtificialHorizonPane
+import ru.cpb9.geotarget.ui.controls.parameters.table.ParametersTable
+import ru.cpb9.geotarget.ui.controls.parameters.tree.ParametersTree
+import ru.cpb9.geotarget.ui.{LayersList, Widget, AddDeviceWidget, GeoTargetModel}
+import ru.cpb9.geotarget.ui.controls.{DeviceList, WorldWindNode}
+import ru.cpb9.geotarget.ui.layers.{GraticuleLayer, DeviceTailsLayer, DevicesLayer}
 import ru.cpb9.geotarget.{Messages, SimpleDeviceRegistry, SimpleDeviceController, ActorsRegistry}
 import ru.cpb9.geotarget.akka.ActorName
 import ru.cpb9.geotarget.akka.server.TmServerActor
+import scala.collection.JavaConversions._
+
+import scalafx.application.{Platform, JFXApp}
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.beans.Observable
+import scalafx.event.ActionEvent
+import scalafx.scene.Scene
+import scalafx.scene.control.{CheckMenuItem, MenuBar, Menu, MenuItem}
+import scalafx.Includes._
+import scalafx.scene.layout.{Priority, VBox, Pane}
 
 /**
   * @author Artem Shein
   */
-object GeoTargetApplication extends Application with LazyLogging {
-/*
+object GeoTargetApplication extends JFXApp with StrictLogging {
+
   C10N.configure(new DefaultC10NAnnotations)
   Locale.setDefault(new Locale("ru"))
 
@@ -29,106 +42,68 @@ object GeoTargetApplication extends Application with LazyLogging {
   val deviceController = new SimpleDeviceController(SimpleDeviceRegistry.newInstance(tmServerActorRef),
     new WorldWindNode(new GeoTargetModel()))
 
-  def main(args: Array[String]): Unit = {
-    try {
-      launch(args)
-    } catch {
-      case e: Throwable =>
-        logger.error(ExceptionUtils.getStackTrace(e))
-        System.exit(1)
-    }
-    System.exit(0)
+  deviceController.getWorldWind.getPanel.getModel.getLayers.addAll(Seq(
+    new DevicesLayer(I.devices(), deviceController),
+    new DeviceTailsLayer(I.deviceTail(), deviceController),
+    new GraticuleLayer(I.graticule())
+  ))
+
+  val addDeviceWidget = new AddDeviceWidget(deviceController, tmServerActorRef)
+  val parametersTreeWidget = new Widget(I.parametersTree(), new ParametersTree(deviceController))
+  val deviceListWidget = new Widget(I.deviceList(), new DeviceList(deviceController, tmServerActorRef))
+  val artificialHorizonWidget = new Widget(I.artificialHorizon(), new ArtificialHorizonPane(deviceController.getDeviceRegistry))
+  val worldWindNode = deviceController.getWorldWind
+  val layerListWidget = new Widget(I.layerList(), new LayersList(worldWindNode))
+
+  val fileMenu = new Menu(I.file()) {
+    items = Seq(
+      new MenuItem(I.exit()) {
+        onAction = (ae: ActionEvent) => requestShutdown()
+      })
+  }
+  val viewMenu = new Menu(I.view()) {
+    items = Seq(
+      newBindedToWidgetVisibilityCheckMenuItem(addDeviceWidget),
+      newBindedToWidgetVisibilityCheckMenuItem(parametersTreeWidget),
+      newBindedToWidgetVisibilityCheckMenuItem(deviceListWidget),
+      newBindedToWidgetVisibilityCheckMenuItem(artificialHorizonWidget),
+      newBindedToWidgetVisibilityCheckMenuItem(layerListWidget))
   }
 
-  def start(primaryStage: Stage): Unit = {
+  val mainMenu = new MenuBar() {
+    menus ++= Seq(fileMenu, viewMenu)
+  }
 
-  }*/
-/*
-private static final Logger LOG = LoggerFactory.getLogger(GeoTargetApplication.class);
+  val mainPane = new Pane() {
+    children.addAll(Seq(worldWindNode, addDeviceWidget, parametersTreeWidget, deviceListWidget,
+      artificialHorizonWidget, layerListWidget))
+  }
 
-    @Override
-    public void start(@NotNull Stage primaryStage) throws Exception
-    {
-        LOG.info("Starting...");
-        List<String> parameters = getParameters().getUnnamed();
+  val sizeUpdater = (o: Observable) => {
+    worldWindNode.resize(mainPane.getWidth, mainPane.getHeight)
+    worldWindNode.getPanel.setSize(mainPane.getWidth.toInt, mainPane.getHeight.toInt)
+  }
 
-        deviceController.getWorldWind().getPanel().getModel().getLayers().add(new DevicesLayer(I.devices(), deviceController));
-        deviceController.getWorldWind().getPanel().getModel().getLayers().add(
-                new DeviceTailsLayer(I.deviceTail(), deviceController));
-        deviceController.getWorldWind().getPanel().getModel().getLayers().add(new GraticuleLayer(I.graticule()));
+  mainPane.height.onInvalidate(sizeUpdater)
+  mainPane.width.onInvalidate(sizeUpdater)
 
-        Widget addDeviceWidget = new AddDeviceWidget(deviceController, tmServerActorRef);
-        //Widget parametersTableWidget = new Widget(I.parametersTable(), new ParametersTable(deviceController));
-        Widget parametersTreeWidget = new Widget(I.parametersTree(), new ParametersTree(deviceController));
-        Widget deviceListWidget = new Widget(I.deviceList(), new DeviceList(deviceController, tmServerActorRef));
-        Widget artificialHorizonWidget =
-                new Widget(I.artificialHorizon(), new ArtificialHorizonPane(deviceController.getDeviceRegistry()));
-        WorldWindNode worldWindNode = deviceController.getWorldWind();
-        Widget layerListWidget = new Widget(I.layerList(), new LayersList(worldWindNode));
+  val vBox = new VBox(mainMenu, mainPane)
+  VBox.setVgrow(mainPane, Priority.Always)
+  stage = new PrimaryStage {
+    scene = new Scene(vBox, 1200, 1000)
+    title = I.appName()
+  }
 
-        MenuBar mainMenu = new MenuBar();
-        Menu fileMenu = new Menu(I.file());
-        MenuItem exitMenuItem = new MenuItem(I.exit());
-        exitMenuItem.setOnAction(e -> requestShutdown());
-        fileMenu.getItems().addAll(exitMenuItem);
+  def newBindedToWidgetVisibilityCheckMenuItem(widget: Widget): CheckMenuItem = {
+    val menuItem = new CheckMenuItem(widget.getTitle)
+    widget.visibleProperty().bind(menuItem.selectedProperty())
+    widget.getCloseButton.setOnAction((e: ActionEvent) => menuItem.setSelected(false))
+    menuItem
+  }
 
-        Menu viewMenu = new Menu(I.view());
-        CheckMenuItem parametersTableWidgetMenuItem = new CheckMenuItem(I.parametersTable());
-        //parametersTableWidget.visibleProperty().bind(parametersTableWidgetMenuItem.selectedProperty());
-        viewMenu.getItems().addAll(
-                newBindedToWidgetVisibilityCheckMenuItem(addDeviceWidget),
-                //newBindedToWidgetVisibilityCheckMenuItem(parametersTableWidget),
-                newBindedToWidgetVisibilityCheckMenuItem(parametersTreeWidget),
-                newBindedToWidgetVisibilityCheckMenuItem(deviceListWidget),
-                newBindedToWidgetVisibilityCheckMenuItem(artificialHorizonWidget),
-                newBindedToWidgetVisibilityCheckMenuItem(layerListWidget));
-
-        mainMenu.getMenus().addAll(fileMenu, viewMenu);
-
-        Pane mainPane = new Pane();
-        mainPane.getChildren().addAll(worldWindNode, addDeviceWidget,
-                //parametersTableWidget,
-                parametersTreeWidget,
-                deviceListWidget,
-                artificialHorizonWidget,
-                layerListWidget);
-
-        InvalidationListener sizeUpdater = (e) -> {
-            worldWindNode.resize(mainPane.getWidth(), mainPane.getHeight());
-            worldWindNode.getPanel().setSize((int) mainPane.getWidth(), (int) mainPane.getHeight());
-        };
-        mainPane.heightProperty().addListener(sizeUpdater);
-        mainPane.widthProperty().addListener(sizeUpdater);
-
-        VBox vBox = new VBox(mainMenu, mainPane);
-        VBox.setVgrow(mainPane, Priority.ALWAYS);
-        primaryStage.setScene(new Scene(vBox, 1200, 1000));
-        primaryStage.setTitle(I.appName());
-        primaryStage.show();
-
+  def requestShutdown() {
+    try ACTORS_REGISTRY.shutdown() finally {
+      Platform.exit()
     }
-
-    @NotNull
-    private static CheckMenuItem newBindedToWidgetVisibilityCheckMenuItem(@NotNull Widget widget)
-    {
-        CheckMenuItem menuItem = new CheckMenuItem(widget.getTitle());
-        widget.visibleProperty().bind(menuItem.selectedProperty());
-        widget.getCloseButton().setOnAction((e) -> menuItem.setSelected(false));
-        return menuItem;
-    }
-
-    public void requestShutdown()
-    {
-        LOG.info("Shutdown requested, exiting");
-        try
-        {
-            ACTORS_REGISTRY.shutdown();
-        }
-        finally
-        {
-            Platform.exit();
-        }
-    }
- */
-  override def start(primaryStage: Stage) {}
+  }
 }
