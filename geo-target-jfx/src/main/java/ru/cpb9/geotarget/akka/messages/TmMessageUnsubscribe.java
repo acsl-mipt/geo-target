59612083423f4a057d3468bf064b984c0da38789
Artem Shein <metadeus@aisys.ru>
2016-04-05 19:21:05 +0300

    Refactoring

--- geo-target-jfx/src/main/java/ru/cpb9/geotarget/akka/messages/TmMessageUnsubscribe.java
+++ geo-target-jfx/src/main/java/ru/cpb9/geotarget/akka/messages/TmMessageUnsubscribe.java
@@ -2,7 +2,7 @@ package ru.cpb9.geotarget.akka.messages;
 
 import org.jetbrains.annotations.NotNull;
 import ru.cpb9.geotarget.DeviceGuid;
-import ru.mipt.acsl.decode.model.domain.pure.component.messages.TmMessage;
+import ru.mipt.acsl.decode.model.domain.pure.component.message.TmMessage;
 
 /**
  * @author Artem Shein
